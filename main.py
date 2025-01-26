e54aa05a897d459296cc63253127574a2bdb6c2c
primequantuM4 <gediezra@gmail.com>
2025-01-26 15:31:53 +0300

    fix: nltk resource

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ import numpy as np
 import pickle
 from flask import Flask, request, jsonify
 
-nltk.download('punkt_tab')
+# Set custom path for NLTK data
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
+# Download required resources
+nltk.download('wordnet', download_dir=nltk_data_path)
+nltk.download('punkt', download_dir=nltk_data_path)
+
 
 # Define the Flask app
 app = Flask(__name__)
