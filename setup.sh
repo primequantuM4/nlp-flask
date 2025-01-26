6b389a74835f063a46a66d1fe5dbe09f28d26c9b
primequantuM4 <gediezra@gmail.com>
2025-01-26 18:44:28 +0300

    trying without docker

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo "Helllooooooooooooooooooooo worlddddddddddd"
-mkdir -p /opt/render/project/src/nltk_data
 export NLTK_DATA=/opt/render/project/src/nltk_data
-python -m nltk.downloader all
 
