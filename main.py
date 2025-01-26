a6ab95a4ba147f5f845aea1564fdf814fdeb9ad6
primequantuM4 <gediezra@gmail.com>
2025-01-26 18:14:29 +0300

    trying ports

--- main.py
+++ main.py
@@ -91,5 +91,5 @@ def predict():
 
 # Run the Flask app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
